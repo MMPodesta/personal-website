23a429ec492cd6971bd685abaf366a239718b4ea
MauricioMurta <mauriciomurta13@hotmail.com>
2022-06-04 19:33:17 -0300

    updates

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://kit.fontawesome.com/28528889ff.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"> 
     <link href="style.css" rel="stylesheet">
     <script defer src="script.js"></script>
--- index.php
+++ index.php
@@ -7,7 +7,21 @@ function customPageHeader(){?>
 include_once('header.php');
 ?>
 <!------------body starts----------->
-
+<div class="landing-image">
+    <div class="landing-text">
+    <h1>test main</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur 
+    adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation
+    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit
+    esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, 
+    sunt in culpa qui officia deserunt mollit anim id est 
+    laborum.</p>
+    </div>
+</div>
 
 
 
